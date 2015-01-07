5564f0ca7dbcccc101aa3ed0512b6d8bdfcfc16e
Don J <kahdojay@gmail.com>
2015-01-07 00:52:37 -0500

    Add elapsed_time helper method.

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,15 @@
 class Tweet < ActiveRecord::Base
   belongs_to :author, class_name: "User"
+
+  def elapsed_time
+    seconds = (Time.now - self.created_at).round(0)
+    case
+    when seconds < 60
+      "#{seconds}s"
+    when seconds > 60
+      "#{seconds / 60}m"
+    when seconds > 3600
+      "#{seconds / 3600}h"
+    end
+  end
 end
