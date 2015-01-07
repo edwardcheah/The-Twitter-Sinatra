dfa9f5c5dd45c32fd2212cc72506d1ed4c9a66e1
Don J <kahdojay@gmail.com>
2015-01-07 00:39:31 -0500

    Make better seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,2 @@
-5.times { User.create(user_name: 'a', password: 'a') }
+('A'..'Z').to_a.each { |letter| User.create(user_name: "#{letter}", password: "#{letter}") }
 User.all.each { |user| user.tweets << Tweet.create(content: "a tweet by #{user.user_name}") }
